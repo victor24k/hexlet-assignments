f1cbf51a4bede0f133ec3c2d7e205a720f9543c7
victor24k <ondewhfe@yandex.kz>
2024-12-15 20:53:02 +0000

    Write tests

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,7 +5,31 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
-  
+  def setup
+    @stack = Stack.new [1, 2, 3]
+  end
+
+  def test_push_adds_element_on_the_stack
+    elem = 4
+    @stack.push! elem
+
+    assert { @stack.to_a.last == elem }
+  end
+
+  def test_pop_deletes_element_from_the_stack
+    assert { @stack.to_a.last == @stack.pop! }
+  end
+
+  def test_clear_makes_stack_empty
+    assert { @stack.clear!.empty? }
+  end
+
+  def test_empty_works_correctly
+    assert { !@stack.empty? }
+
+    @stack.clear!
+    assert { @stack.empty? }
+  end
   # END
 end
 
